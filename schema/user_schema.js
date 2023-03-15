734888caa6eb5b004976db3651007007e310911e
Mithilesh982 <127495653+Mithilesh982@users.noreply.github.com>
2023-03-15 13:24:33 +0530

    Update user_schema.js

--- schema/user_schema.js
+++ schema/user_schema.js
@@ -13,8 +13,8 @@ const userschema = mongoose.Schema({
 })
 
 autoIncrement.initialize(mongoose.connection);
-userschema.plugin(autoIncrement.plugin,"teamscylla_usercontact")
+userschema.plugin(autoIncrement.plugin,"teamscylla_user")
 
-const user = mongoose.model("teamscylla_usercontact",userschema )
+const user = mongoose.model("teamscylla_user",userschema )
 
 export default user;
